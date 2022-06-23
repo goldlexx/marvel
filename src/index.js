3dc302b14d3d48ff7caca44b24adf0db6bed7dbe
Ruslan Yuldashev <rustik.yuldashev@yandex.ru>
2022-06-23 22:09:17 +0300

    Добавить скелетон и загрузку персонажа по клику

--- src/index.js
+++ src/index.js
@@ -3,13 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './components/app/App';
 import './style/style.scss';
 
-
-
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,7 +1,6 @@
 class MarvelService {
-
-    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-    _apiKey = 'apikey=f654ffdb901c4c6f44993842aeb3174e';
+  _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+  _apiKey = 'apikey=f654ffdb901c4c6f44993842aeb3174e';
 
   getResource = async (url) => {
     let res = await fetch(url);
@@ -14,25 +13,30 @@ class MarvelService {
   };
 
   getAllCharacters = async () => {
-    const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    const res = await this.getResource(
+      `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
+    );
     return res.data.results.map(this._transformCharacter);
   };
 
   getCharacter = async (id) => {
-   const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
-   return this._transformCharacter(res.data.results[0]);
+    const res = await this.getResource(
+      `${this._apiBase}characters/${id}?${this._apiKey}`
+    );
+    return this._transformCharacter(res.data.results[0]);
   };
 
   _transformCharacter = (char) => {
     return {
+      id: char.id,
       name: char.name,
       description: char.description,
       thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
       homepage: char.urls[0].url,
       wiki: char.urls[1].url,
-    }
-  }
-
+      comics: char.comics.items,
+    };
+  };
 }
 
 export default MarvelService;
