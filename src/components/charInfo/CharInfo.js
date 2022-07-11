ceeb172ecac2c36b04e4e50551d4789c3cebd462
Ruslan Yuldashev <rustik.yuldashev@yandex.ru>
2022-07-11 15:29:15 +0300

    Добавить дозагрузку карточек

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,123 +1,129 @@
-import { Component } from 'react/cjs/react.production.min';
+import { Component } from 'react';
+
+import MarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import Skeleton from '../skeleton/Skeleton';
-import MarvelService from '../../services/MarvelService';
+
 import './charInfo.scss';
 
 class CharInfo extends Component {
-  state = {
-    char: null,
-    loading: false,
-    error: false,
-  };
 
-  marvelService = new MarvelService();
-
-  componentDidMount() {
-    this.updateChar();
-  }
-
-  componentDidUpdate(prevProps) {
-    if (this.props.charId !== prevProps.charId) {
-      this.updateChar();
+    state = {
+        char: null,
+        loading: false,
+        error: false
     }
-  }
 
-  updateChar = () => {
-    const { charId } = this.props;
-    if (!charId) {
-      return;
+    marvelService = new MarvelService();
+
+    componentDidMount() {
+        this.updateChar();
     }
-    this.onCharLoading();
-    this.marvelService
-      .getCharacter(charId)
-      .then(this.onCharLoaded)
-      .catch(this.onError);
-  };
 
-  onCharLoaded = (char) => {
-    this.setState({
-      char,
-      loading: false,
-    });
-  };
+    componentDidUpdate(prevProps){
+        if (this.props.charId !== prevProps.charId) {
+            this.updateChar();
+        }
+    }
 
-  onCharLoading = () => {
-    this.setState({
-      loading: true,
-    });
-  };
+    updateChar = () => {
+        const {charId} = this.props;
+        if (!charId) {
+            return;
+        }
 
-  onError = () => {
-    this.setState({
-      loading: false,
-      error: true,
-    });
-  };
+        this.onCharLoading();
 
-  render() {
-    const { char, loading, error } = this.state;
+        this.marvelService
+            .getCharacter(charId)
+            .then(this.onCharLoaded)
+            .catch(this.onError);
+    }
 
-    const skeleton = char || loading || error ? null : <Skeleton />;
-    const errorMessage = error ? <ErrorMessage /> : null;
-    const spinner = loading ? <Spinner /> : null;
-    const content = !(loading || error || !char) ? <View char={char} /> : null;
+    onCharLoaded = (char) => {
+        this.setState({
+            char, 
+            loading: false
+        })
+    }
 
-    return (
-      <div className='char__info'>
-        {skeleton}
-        {errorMessage}
-        {spinner}
-        {content}
-      </div>
-    );
-  }
+    onCharLoading = () => {
+        this.setState({
+            loading: true
+        })
+    }
+
+    onError = () => {
+        this.setState({
+            loading: false,
+            error: true
+        })
+    }
+
+    render() {
+        const {char, loading, error} = this.state;
+
+        const skeleton = char || loading || error ? null : <Skeleton/>;
+        const errorMessage = error ? <ErrorMessage/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = !(loading || error || !char) ? <View char={char}/> : null;
+
+        return (
+            <div className="char__info">
+                {skeleton}
+                {errorMessage}
+                {spinner}
+                {content}
+            </div>
+        )
+    }
 }
 
-const View = ({ char }) => {
-  const { name, description, thumbnail, homepage, wiki, comics } = char;
-  let imgStyle = { objectFit: 'cover' };
-  if (
-    thumbnail ===
-    'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
-  ) {
-    imgStyle = { objectFit: 'unset' };
-  }
+const View = ({char}) => {
+    const {name, description, thumbnail, homepage, wiki, comics} = char;
 
-  return (
-    <>
-      <div className='char__basics'>
-        <img src={thumbnail} alt={name} style={imgStyle} />
-        <div>
-          <div className='char__info-name'>{name}</div>
-          <div className='char__btns'>
-            <a href={homepage} className='button button__main'>
-              <div className='inner'>homepage</div>
-            </a>
-            <a href={wiki} className='button button__secondary'>
-              <div className='inner'>Wiki</div>
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className='char__descr'>{description}</div>
-      <div className='char__comics'>Comics:</div>
-      <ul className='char__comics-list'>
-        {comics.length > 0 ? null : 'There is no comics with this character'}
-        {comics.map((item, i) => {
-          return (
-            <li key={i} className='char__comics-item'>
-              {item.name}
-            </li>
-          );
-        })}
-        <li className='char__comics-item'>
-          All-Winners Squad: Band of Heroes (2011) #3
-        </li>
-      </ul>
-    </>
-  );
-};
+    let imgStyle = {'objectFit' : 'cover'};
+    if (thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = {'objectFit' : 'contain'};
+    }
+
+    return (
+        <>
+            <div className="char__basics">
+                <img src={thumbnail} alt={name} style={imgStyle}/>
+                <div>
+                    <div className="char__info-name">{name}</div>
+                    <div className="char__btns">
+                        <a href={homepage} className="button button__main">
+                            <div className="inner">homepage</div>
+                        </a>
+                        <a href={wiki} className="button button__secondary">
+                            <div className="inner">Wiki</div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div className="char__descr">
+                {description}
+            </div>
+            <div className="char__comics">Comics:</div>
+            <ul className="char__comics-list">
+                {comics.length > 0 ? null : 'There is no comics with this character'}
+                {
+                    comics.map((item, i) => {
+                        // eslint-disable-next-line
+                        if (i > 9) return;
+                        return (
+                            <li key={i} className="char__comics-item">
+                                {item.name}
+                            </li>
+                        )
+                    })
+                }                
+            </ul>
+        </>
+    )
+}
 
 export default CharInfo;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
+
 import './style/style.scss';
 
 ReactDOM.render(
@@ -9,3 +10,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
